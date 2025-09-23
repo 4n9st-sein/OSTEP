d7d991daea2bf0fcc4e2f9e9e0d85a1ad79d2128
4n9st-sein <kappa32tutinoko@gmail.com>
2025-09-23 22:00:39 +0900

    create new file and section

---
+++ section3/source/5.1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    printf("hello (pid: %d)\n", (int) getpid());
+    int rc = fork();
+    if (rc < 0) {
+        // fork failed
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    
+}
